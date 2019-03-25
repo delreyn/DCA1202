e898e6e902da18c10c9516c00d116cf8850c2532
Reyne Jasson <30417318+delreyn@users.noreply.github.com>
2019-03-24 22:29:44 -0300

    Exibir apenas um plano no eixo z

--- 3DmatrixAllocDraw.c
+++ 3DmatrixAllocDraw.c
@@ -3,22 +3,44 @@
 
 int* gerarMatrix(int M, int N, int O);
 void desenha(int* A,int M,int N,int O, int r);
-int melhRaio(int M, int N, int O);
+int melhRaio(int M, int N, int O); //garante 
 int getSmallest(int a, int b, int c);
+void print(int *a, int m, int n, int o);
 
 
 int main()
 {
-   int M=4;
-   int N=4;
-   int O=4;
+   int M=7;
+   int N=7;
+   int O=7;
         
     int* A = gerarMatrix(M,N,O);
     int r = melhRaio(M,N,O);
+    printf("O melhor raio eh: %d \n", r);
     desenha(A,M,N,O,r);
+    print(A,M,N,O);
     free(A);
     
     return 0;
+}
+
+void print(int *a, int m, int n, int o){
+    int i,j,k;
+    int* offset;
+    int K = 2;
+    
+    for(i=0; i<m; i++){
+        for(j=0;j<n;j++){
+            for(k=0;k<o;k++){
+                offset = (a + i*n*o + j*o + k);
+                printf("%d ", *offset);
+            }
+            printf("\n");
+        }
+       printf("\n");
+       break;
+    }
+    
 }
 
 int getSmallest(int a, int b, int c){
@@ -44,17 +66,20 @@ int melhRaio(int M, int N, int O){
 
 void desenha(int* A,int M,int N,int O, int r){
 
-    int* offset;
+// Cria variaveis e offset tal que: M[offset] == M[i][j[k]
+    int* offset; 
     int x0,y0,z0;
+//define o ponto x0, y0, z0 de onde começara a desenha a esfera
     x0 = M/2;
     y0 = N/2;
     z0 = O/2;
 
-
+//percorre todo o array 
     for (int i=0; i<M;i++){
         for(int j=0;j<N; j++){
             for (int k=0; k<O; k++){
                 offset = (A + i*N*O + j*O + k);
+                //caso a distancia do P(x0,y0,z0) e P(xi,yi,zi) <= raio
                 if(sqrt((x0 - i)*(x0 - i) + (y0-j)*(y0-j) + (z0-k)*(z0-k)) <= r ) *offset =  1;
                 else *offset = 0;
             }
